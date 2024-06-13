8bad99ef96e194f14a88de6e27e9fd0b7020ecef
Evan Dimitri <145658103+evandimitri89@users.noreply.github.com>
2024-06-13 16:21:46 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hamburger Menu
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
